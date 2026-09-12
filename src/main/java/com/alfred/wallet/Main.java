277c7c51196730c84e4ee4629f8f20fc67df03d4
Alfred Chatora <siralfredthegreat@sir.alfredthegr8>
2026-09-13 00:05:43 +0200

    Add Customer model

--- src/main/java/com/alfred/wallet/Main.java
+++ src/main/java/com/alfred/wallet/Main.java
@@ -1,7 +1,13 @@
 package com.alfred.wallet;
 
+import com.alfred.wallet.model.Customer;
+
 public class Main {
   public static void main(String[] args) {
 
+    Customer customer = new Customer(10001, "Alfred");
+
+    System.out.printf("ID: %d%n", customer.getCustomerId());
+    System.out.printf("Name: %s%n", customer.getName());
   }
 }
---
+++ src/main/java/com/alfred/wallet/model/Customer.java
@@ -0,0 +1,19 @@
+package com.alfred.wallet.model;
+
+public class Customer {
+  private long customerId;
+  private String name;
+
+  public Customer(long customerId, String name) {
+    this.customerId = customerId;
+    this.name = name;
+  }
+
+  public long getCustomerId() {
+    return customerId;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
